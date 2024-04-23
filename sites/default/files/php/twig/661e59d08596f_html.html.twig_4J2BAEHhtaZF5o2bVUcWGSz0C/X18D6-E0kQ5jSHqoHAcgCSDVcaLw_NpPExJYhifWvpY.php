1f17a4d6ddc77bd909fa06ebf3b0d326915fc559
karanbojappa7 <karanbojappa7@gmail.com>
2024-04-23 18:21:24 +0530

    first commit

---
+++ sites/default/files/php/twig/661e59d08596f_html.html.twig_4J2BAEHhtaZF5o2bVUcWGSz0C/X18D6-E0kQ5jSHqoHAcgCSDVcaLw_NpPExJYhifWvpY.php
@@ -0,0 +1,167 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/evolve/templates/layout/html.html.twig */
+class __TwigTemplate_36eb87aaa2d9b0503b21797d01a418db extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 26
+        echo "<!DOCTYPE html>
+<html";
+        // line 27
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["html_attributes"] ?? null), 27, $this->source), "html", null, true);
+        echo ">
+\t<head>
+\t\t";
+        // line 30
+        echo "\t\t";
+        $context["site_url"] = $this->extensions['Drupal\Core\Template\TwigExtension']->getPath("<current>");
+        // line 31
+        echo "\t\t<title>";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar($this->extensions['Drupal\Core\Template\TwigExtension']->safeJoin($this->env, $this->sandbox->ensureToStringAllowed(($context["head_title"] ?? null), 31, $this->source), " | "));
+        echo "</title>
+\t\t<meta charset=\"utf-8\">
+\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+\t\t<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\">
+\t\t<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js\"></script>
+\t\t<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js\"></script>
+\t\t<link rel=\"stylesheet\" href=\"assets/css/style.css\">
+\t\t<link rel=\"stylesheet\" href=\"assets/css/style_mobile.css\">
+\t\t<link href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css\" rel=\"stylesheet\" />
+\t\t<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css\" />
+\t\t<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css\" />
+\t\t";
+        // line 42
+        echo "\t
+\t\t<head-placeholder token=\"";
+        // line 43
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar($this->sandbox->ensureToStringAllowed(($context["placeholder_token"] ?? null), 43, $this->source));
+        echo "\">
+\t\t<css-placeholder token=\"";
+        // line 44
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar($this->sandbox->ensureToStringAllowed(($context["placeholder_token"] ?? null), 44, $this->source));
+        echo "\">
+\t\t<js-placeholder token=\"";
+        // line 45
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar($this->sandbox->ensureToStringAllowed(($context["placeholder_token"] ?? null), 45, $this->source));
+        echo "\">
+\t\t<body";
+        // line 46
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attributes"] ?? null), 46, $this->source), "html", null, true);
+        echo ">
+\t\t\t<a href=\"#main-content\" class=\"visually-hidden focusable\">
+\t\t\t\t";
+        // line 48
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(t("Skip to main content"));
+        echo "
+\t\t\t</a>
+\t\t\t";
+        // line 50
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["page_top"] ?? null), 50, $this->source), "html", null, true);
+        echo "
+\t\t\t";
+        // line 51
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["page"] ?? null), 51, $this->source), "html", null, true);
+        echo "
+\t\t\t";
+        // line 52
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["page_bottom"] ?? null), 52, $this->source), "html", null, true);
+        echo "
+\t\t\t<js-bottom-placeholder token=\"";
+        // line 53
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar($this->sandbox->ensureToStringAllowed(($context["placeholder_token"] ?? null), 53, $this->source));
+        echo "\">
+    \t</body>
+\t</head>
+</html>
+";
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["html_attributes", "head_title", "placeholder_token", "attributes", "page_top", "page", "page_bottom"]);    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName()
+    {
+        return "themes/custom/evolve/templates/layout/html.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo()
+    {
+        return array (  102 => 53,  98 => 52,  94 => 51,  90 => 50,  85 => 48,  80 => 46,  76 => 45,  72 => 44,  68 => 43,  65 => 42,  50 => 31,  47 => 30,  42 => 27,  39 => 26,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/custom/evolve/templates/layout/html.html.twig", "C:\\xampp\\htdocs\\linqmd\\themes\\custom\\evolve\\templates\\layout\\html.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 30);
+        static $filters = array("escape" => 27, "safe_join" => 31, "raw" => 43, "t" => 48);
+        static $functions = array("path" => 30);
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set'],
+                ['escape', 'safe_join', 'raw', 't'],
+                ['path']
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
